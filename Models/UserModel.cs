af1489fe7cb52110294a9174301e2b18052de4dc
Kauã <kaua.borba03@gmail.com>
2024-11-10 01:00:18 -0300

    Create: Validation model user

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,10 +1,16 @@
-﻿namespace ContactSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactSystem.Models
 {
     public class UserModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo 'Nome' precisa ser preenchido")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "O campo 'E-mail' precisa ser preenchido")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "O campo 'Password' precisa ser preenchido")]
         public string? Password { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime LastUpdatedAt { get; set; }
